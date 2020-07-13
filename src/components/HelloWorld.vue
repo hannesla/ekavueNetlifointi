05bab7337da155e477b8144743f08942dd7834f9
Hannes <hannes.laukkanen@gmail.com>
2020-07-13 16:14:06 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+      {{msg}} <br />
+      {{yksinkertainen}}<br />
+      {{palautettavaaDataa[0].kivaNumero}}
+      <div v-bind:key="tavara.kivaNumero" v-for="tavara in palautettavaaDataa">
+          {{tavara}}
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    yksinkertainen: String
+  },
+  methods: {
+    esimerkkiMetodi() {
+
+    }
+  },
+  data() {
+    return {
+      palautettavaaDataa: 
+        [
+          {
+            kivaNumero: 12345678
+          },
+          {
+            kivaNumero: 123456780
+          }
+        ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
