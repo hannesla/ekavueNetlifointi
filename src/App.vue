67afe0c7c79e7189acd0a6b8afa1c6fc99e3b9db
Hannes <hanneslaukkanen@gmail.com>
2020-07-13 16:43:09 +0300

    Eka päivitys

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" yksinkertainen="yksinkertasta"/>
+    <HelloWorld msg="Hei tää toimii" yksinkertainen="yksinkertaista"/>
   </div>
 </template>
 
