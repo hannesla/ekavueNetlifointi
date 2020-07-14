2bf97304fde56038919fb5f493b6f980014d7431
Hannes <hanneslaukkanen@gmail.com>
2020-07-14 19:00:12 +0300

    soundcloud näyte

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
           {{tavara}}
       </div>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/JcopYjDomao" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/823915705&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/metaru87" title="metaru" target="_blank" style="color: #cccccc; text-decoration: none;">metaru</a> · <a href="https://soundcloud.com/metaru87/tokyo-rocks" title="Tokyo rocks" target="_blank" style="color: #cccccc; text-decoration: none;">Tokyo rocks</a></div>
   </div>
 </template>
 
